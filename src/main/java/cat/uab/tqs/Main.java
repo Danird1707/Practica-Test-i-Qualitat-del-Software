e6c422cd7bea938a825a66a5c60f9032a4e3ef5b
Daniel <danird2305@gmail.com>
2025-11-24 10:35:49 +0100

    Juego

---
+++ src/main/java/cat/uab/tqs/Main.java
@@ -0,0 +1,14 @@
+package cat.uab.tqs;
+
+import cat.uab.tqs.GraficInterface.GameWindow;
+
+/**
+ * Punto de entrada del juego Tetris.
+ */
+public class Main {
+    public static void main(String[] args) {
+        // Lanza la ventana principal del juego
+        new GameWindow();
+    }
+
+}
